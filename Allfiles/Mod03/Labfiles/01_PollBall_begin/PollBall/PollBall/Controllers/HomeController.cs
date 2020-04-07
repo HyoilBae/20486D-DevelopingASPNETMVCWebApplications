e0b2ce64ecc9b5dfcbcaad0a6da7162821cf3f07
HyoBae <59951096+HyoilBae@users.noreply.github.com>
2020-04-07 16:29:12 -0700

    Mod03

---
+++ Allfiles/Mod03/Labfiles/01_PollBall_begin/PollBall/PollBall/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PollBall.Services;
+using System.Text;
+
+namespace PollBall.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IPollResultsService _pollResults;
+
+        public HomeController(IPollResultsService pollResults)
+        {
+            _pollResults = pollResults;
+        }
+
+        public IActionResult Index()
+        {
+            //return View();
+            //return Content("Hello from controller.");
+            if (Request.Query.ContainsKey("submitted"))
+            {
+                StringBuilder results = new StringBuilder();
+                SortedDictionary<SelectedGame, int> voteList = _pollResults.GetVoteResult();
+
+                foreach (var gameVotes in voteList)
+                {
+                    results.Append($"Game name: {gameVotes.Key}. Vote: {gameVotes.Value}{Environment.NewLine}");
+                }
+
+                return Content(results.ToString());
+            }
+            else
+            {
+                return Redirect("poll-questions.html");
+            }
+        }
+    }
+}
